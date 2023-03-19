93ac19cd5af23a5d54004271c604687014bea9aa
Halil Olcay <holcay92@gmail.com>
2023-03-20 00:11:02 +0300

    Date picker added

--- app/src/main/java/com/example/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/example/happyplaces/AddHappyPlaceActivity.kt
@@ -1,11 +1,20 @@
 package com.example.happyplaces
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.happyplaces.databinding.ActivityAddHappyPlaceBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
-class AddHappyPlaceActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityAddHappyPlaceBinding
+class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
+
+
+
+private lateinit var binding: ActivityAddHappyPlaceBinding
+    private var cal = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
@@ -16,5 +25,37 @@ class AddHappyPlaceActivity : AppCompatActivity() {
         binding.toolbarAddPlace.setNavigationOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
+        dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDateInView()
+
+        }
+        // we need to set the click listener for the edit text
+        // because we want to show the date picker dialog when the user clicks on the edit text.
+        // after clicking on the edit text we will show the date picker dialog via the click listener.
+        binding.etDate.setOnClickListener(this)
+
+    }
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.et_date -> {
+                DatePickerDialog(
+                    this@AddHappyPlaceActivity,
+                    dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+        }
+
+    }
+
+    private fun updateDateInView() {
+        val myFormat = "dd.MM.yyyy" // mention the format you need
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+        binding.etDate.setText(sdf.format(cal.time).toString())
     }
 }
