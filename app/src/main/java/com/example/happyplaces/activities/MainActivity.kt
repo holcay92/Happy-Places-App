3eaf92060400c76801c1f69c9812e265270e4181
Halil Olcay <holcay92@gmail.com>
2023-03-21 01:56:23 +0300

    recyclerView added. items now can be seen on main activity. But app is freezing!

--- app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.happyplaces.adapters.HappyPlacesAdapter
 import com.example.happyplaces.database.DatabaseHandler
 import com.example.happyplaces.databinding.ActivityMainBinding
 import com.example.happyplaces.models.HappyPlaceModel
@@ -23,16 +25,30 @@ class MainActivity : AppCompatActivity() {
         }
         getHappyPlacesListFromLocalDB()
     }
+    private fun setupHappyPlacesRecyclerView(happyPlaceList: ArrayList<HappyPlaceModel>) {
 
+        binding?.rvHappyPlacesList?.layoutManager = LinearLayoutManager(this)
+
+        binding?.rvHappyPlacesList?.setHasFixedSize(true)
+
+        val placesAdapter = HappyPlacesAdapter(this, happyPlaceList)
+        binding?.rvHappyPlacesList?.adapter = placesAdapter
+
+    }
     private fun getHappyPlacesListFromLocalDB() {
         val dbHandler = DatabaseHandler(this)
         val getHappyPlaceList: ArrayList<HappyPlaceModel> = dbHandler.getHappyPlacesList()
 
         if (getHappyPlaceList.size > 0) {
           for(i in getHappyPlaceList){
-              Log.e("Title", i.title)
-                Log.e("Description", i.description)
+            binding?.rvHappyPlacesList?.visibility = android.view.View.VISIBLE
+            binding?.tvNoRecordsAvailable?.visibility = android.view.View.GONE
+            setupHappyPlacesRecyclerView(getHappyPlaceList)
           }
+        }
+        else{
+            binding?.rvHappyPlacesList?.visibility = android.view.View.GONE
+            binding?.tvNoRecordsAvailable?.visibility = android.view.View.VISIBLE
         }
     }
 }
